602a448e045f1676dfa9b0ce3d43e22bff186ca7
cayang <cayang@simenvi.com.tw>
2022-12-28 11:55:10 +0800

    feat: add post router

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 const express = require("express")
 const exphbs = require("express-handlebars")
+const users = require("./users")
 const port = 3000
 
 const app = express()
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: "hbs" }))
 app.set("view engine", "hbs")
+app.use(express.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => {
   res.render("index")
+})
+
+app.post("/", (req, res) => {
+  const { Email, Password } = req.body
+  const User = users.find(user => {
+    return user.email.includes(Email) && user.password.includes(Password)
+  })
+  console.log(User)
+  if (User) {
+    const message = `Welcome Back, ${User.firstName}`
+    res.render("welcome", { message })
+  } else {
+    const error = "Username 或 Password 錯誤"
+    res.render("index", { error })
+  }
 })
 
 app.listen(port, () => {
